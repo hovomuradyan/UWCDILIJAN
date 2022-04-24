259b2940cc13f2023fa7004b68d106b7bd3b30e2
hovomuradyan <88653435+hovomuradyan@users.noreply.github.com>
2022-04-24 13:49:18 +0400

    Update WhiteSweet.java

--- WhiteSweet.java
+++ WhiteSweet.java
@@ -4,10 +4,6 @@ public class WhiteSweet extends White {
     public void printType() {
         System.out.println("Spitak Qagcr");
     }
-    int countOfBottle = 0;
-    public void addBottles(int n){
-        countOfBottle+=n;
-    }
     public void printBottleCount(){
         System.out.println(countOfBottle);
     }
