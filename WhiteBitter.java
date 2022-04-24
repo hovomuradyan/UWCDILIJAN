7e369ee77e321fd9611bcb6bdcd14ab4e4188b39
hovomuradyan <88653435+hovomuradyan@users.noreply.github.com>
2022-04-24 13:49:04 +0400

    Update WhiteBitter.java

--- WhiteBitter.java
+++ WhiteBitter.java
@@ -4,10 +4,6 @@ public class WhiteBitter extends White {
     public void printType() {
         System.out.println("Spitak Dary");
     }
-    int countOfBottle = 0;
-    public void addBottles(int n){
-        countOfBottle+=n;
-    }
     public void printBottleCount(){
         System.out.println(countOfBottle);
     }
