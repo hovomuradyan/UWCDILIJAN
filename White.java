7035c494b89ae8ebad41075ade9317ca272f5f1a
hovomuradyan <88653435+hovomuradyan@users.noreply.github.com>
2022-04-24 13:48:46 +0400

    Update White.java

--- White.java
+++ White.java
@@ -4,10 +4,6 @@ public class White extends Wine{
     public void printType() {
         System.out.println("Spitak Gini");
     }
-    int countOfBottle = 0;
-    public void addBottles(int n){
-        countOfBottle+=n;
-    }
     public void printBottleCount(){
         System.out.println(countOfBottle);
     }
